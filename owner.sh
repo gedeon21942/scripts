7453956cef983e5899a6cf71671c6d6361ad5df1
gedeon21942 <gedeon21942@gmail.com>
2025-06-08 15:24:50 -0400

    Add files via upload

--- owner.sh
+++ owner.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo chown -R $(whoami):$(whoami) Arch.sh backup.sh install.sh rembackup.sh owner.sh remmina.sh scripts.sh unraid.sh tk.sh Keybinds.conf .zshrc 
-sudo chmod +x Arch.sh backup.sh install.sh rembackup.sh owner.sh remmina.sh scripts.sh unraid.sh tk.sh startwork.sh stopwork.sh tinker_screen.py unraid.sh uuid.sh rdp.sh
+sudo chown -R $(whoami):$(whoami) ~/.local/share/scripts
+chmod -R +x ~/.local/share/scripts
+chmod +x ~/.local/share/scripts/*.sh ~/.local/share/scripts/*.py
