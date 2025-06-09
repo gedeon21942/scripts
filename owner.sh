83f883497833012734647ebb554096ba183fa19f
gedeon21942 <gedeon21942@gmail.com>
2025-06-08 20:48:16 -0400

    Add files via upload

--- owner.sh
+++ owner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo chown -R $(whoami):$(whoami) ~/.local/share/scripts
 
-# Make all .sh and .py files executable, but skip Keybinds.conf
-find ~/.local/share/scripts -type f \( -name "*.sh" -o -name "*.py" \) ! -name "Keybinds.conf" -exec chmod +x {} \;
+# Make all .sh and .py files executable, but skip Keybinds.conf and credentials_unraid
+find ~/.local/share/scripts -type f \( -name "*.sh" -o -name "*.py" \) ! -name "Keybinds.conf" ! -name "credentials_unraid" -exec chmod +x {} \;
