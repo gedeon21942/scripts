4843fc64b99814221050b9ad4a0e699b92bcb408
gedeon21942 <gedeon21942@gmail.com>
2026-01-29 11:46:07 -0500

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Send Wake-on-LAN packet
-wol_mac="7c:10:c9:22:27:4F"
-wakeonlan $wol_mac
-
 # Check if the machine is already online
 if ping -c 1 -W 1 192.168.1.80 >/dev/null 2>&1; then
     echo "Machine is already online, skipping sleep."
 else
+    # Send Wake-on-LAN packet
+    wol_mac="7c:10:c9:22:27:4F"
+    wakeonlan $wol_mac
     echo "Waiting for the machine to wake up..."
     sleep 25
 fi
